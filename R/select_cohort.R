549eb3476ff368a9f999fb6af9af6589a1c23d20
Michael Luu <mluu921@gmail.com>
2022-09-15 12:11:18 -0700

    fix bug - function wasn't correctly using the initial timepoint for subsetting

--- R/select_cohort.R
+++ R/select_cohort.R
@@ -1,13 +1,18 @@
 
 
+# data <- read_csv(here('data/shiny_qol_data.csv'), show_col_types = F)
+
 select_cohort <- \(data, selected_treatment, selected_symptom, selected_timepoints, selected_response) {
   data %>%
     filter(trt == selected_treatment) %>%
     filter(symptom_labels == selected_symptom) %>%
+    filter(time_point >= selected_timepoints[[1]] & time_point <= selected_timepoints[[2]]) %>% 
     group_by(patientid) %>%
     filter(first(time_point == selected_timepoints[[1]]) &
              first(response) == selected_response) %>%
-    ungroup() %>%
-    filter(time_point <= selected_timepoints[[2]])
+    ungroup()
   
 }
+
+
+# select_cohort(data, 'Anastrozole', 'Hot Flashes', c(6, 24), 'Moderately')
